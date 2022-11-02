9f33abe8117fa4e28b7084e85c83b4b31e841d03
raphael.nascimento <nascimento.rapha@hotmail.com>
2022-11-01 22:21:22 -0300

    changed view on application run

--- ControleVendas/Program.cs
+++ ControleVendas/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ControleVendas.br.com.projeto.view;
 
 namespace ControleVendas
 {
@@ -16,7 +17,7 @@ namespace ControleVendas
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Frmclientes());
         }
     }
 }
