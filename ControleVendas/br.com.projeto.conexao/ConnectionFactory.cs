3dc1aa669ff4ad49a79241a81b1775545e325c63
raphael.nascimento <nascimento.rapha@hotmail.com>
2022-11-06 15:49:14 -0300

    created MySql connection class

---
+++ ControleVendas/br.com.projeto.conexao/ConnectionFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace ControleVendas.br.com.projeto.conexao
+{
+    public class ConnectionFactory
+    {
+        //connection method to database
+        public MySqlConnection getconnection()
+        {
+            string conexao = ConfigurationManager.ConnectionStrings["bdvendas"].ConnectionString;
+
+            return new MySqlConnection(conexao);
+        }
+
+    }
+}
